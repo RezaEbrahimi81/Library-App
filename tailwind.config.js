91c3fac4c4f0adef1cab043d95be998eae271e89
RezaEbrahimi81 <r3z4.3br4h1m1@gmail.com>
2024-10-24 13:03:28 +0330

    header don

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding:{
+        DEFAULT: '15PX',
+  
+      }
+  
+    },
+    screens:{
+      sm: '640px',
+      md: '768px',
+      xl: '1280px',
+    },
+    fontFamily:{
+      primary: 'dana',
+      secondary: 'Tahoma',
+    },
+  
+    extend: {
+      colors: {
+        primary: '#FF4F5A',
+        secondary: '#6B6B6B',
+      },
+      transitionProperty: {
+        'height': 'height'
+      },
+    },
+  
+    variants: {
+      height: ['responsive', 'hover', 'focus']
+   },
+
+  plugins: [],
+  }}
+
+
